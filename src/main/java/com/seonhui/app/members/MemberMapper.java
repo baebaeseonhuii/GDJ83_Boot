024ad6e1fe3da9706d042919848519c5c3bd4d7b
baebaeseonhuii <qotjsgml3@naver.com>
2024-09-11 17:47:22 +0900

    .

--- src/main/java/com/seonhui/app/members/MemberMapper.java
+++ src/main/java/com/seonhui/app/members/MemberMapper.java
@@ -4,8 +4,11 @@ import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
+//repository 말고 mapper를 쓰는 이유: mapper 어노테이션이 xml파일의 위치도 지정해줌(properties에서도 지정)
 @Mapper
 public interface MemberMapper {
+	
+	//interface 쓰는 이유
 	
 	public int add(MemberVO memberVO)throws Exception;
 	
